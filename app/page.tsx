7e84580e378cb24c32ccb2b6d3f071430fd01273
kirsten <kirstensutantyo@gmail.com>
2025-02-18 18:51:13 -0500

    Reverted main header vh

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Poster from "./assets/images/coexistence.png";
 export default function Home() {
   return (
     <main>
-      <div className="w-full h-[47vh] max-sm:h-[50vh] relative">
+      <div className="w-full h-[60vh] max-sm:h-[50vh] relative">
         <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-white/70  to-white/0 to-10%"></div>
         {/* Text Overlay */}
         <div className="absolute inset-0 flex flex-col items-center justify-center z-10 bg-primary text-white">

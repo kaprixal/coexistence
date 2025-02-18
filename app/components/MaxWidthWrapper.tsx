7bf9d75416dc32cfe7e86d5c6051ed9aa2948258
Mapsee <mapsee26@gmail.com>
2025-02-18 01:55:14 -0500

    added px so content does not hug the sides of screen

--- app/components/MaxWidthWrapper.tsx
+++ app/components/MaxWidthWrapper.tsx
@@ -9,7 +9,7 @@ const MaxWidthWrapper = ({
     children: ReactNode
 }) => {
     return (
-        <div className={cn("mx-auto w-full max-w-screen-2xl", className)}>
+        <div className={cn("mx-auto w-full max-w-screen-2xl px-8", className)}>
             {children}
         </div>
     )

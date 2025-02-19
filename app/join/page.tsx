1704469d67042117c18f84ac6c9bb1a46c283e12
kirsten <kirstensutantyo@gmail.com>
2025-02-19 12:16:33 -0500

    .gitignore fix

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -18,25 +18,25 @@ export default function Home() {
         <h1 className="mt-20 max-sm:text-3xl max-sm:text-center text-primary text-4xl font-bold max-md:text-6xl text-center">
           Get involved with the UofT AI Ethics Hackathon 2025!
         </h1>
-        <p className="mt-5 text-2xl">
+        <p className="mt-5 text-2xl text-center">
           We are looking for passionate individuals to help make this event a success! Whether you want to create or judge groundbreaking projects, inspire as a speaker, mentor the next generation of innovators, sponsor our event or volunteer behind the scenes, there is a place for you at UofT AI Ethics Hackathon 2025.</p><p className="mt-5 text-2xl"> 
           This is your chance to engage with top talent, share your expertise, and contribute to the future of ethical AI.
         </p>
         <div className="mt-10 flex flex-col md:flex-row w-full justify-center items-center">
           <a
-            className="mx-10 mt-5 flex justify-center items-center text-center text-white bg-primary rounded-full px-10 py-3 max-sm:text-xl text-2xl bt font-extrabold duration-50 hover:scale-105 active:scale-85 hover:bg-[#8B5050] uppercase" target="_blank"
+            className="mx-10 mt-5 flex justify-center items-center text-center text-white bg-secondary rounded-full px-10 py-3 max-sm:text-xl text-2xl bt font-extrabold duration-50 hover:scale-105 active:scale-85 hover:bg-[#A1E6EB] uppercase" target="_blank"
             href="https://forms.gle/NyEQv9QDYCnELa888"
           >
             INDUSTRY/ACADEMIC GUEST SIGN UP
           </a>
           <a
-            className="mt-5 flex justify-center items-center text-center text-white bg-secondary rounded-full px-10 py-3 max-sm:text-xl text-2xl bt font-extrabold duration-50 hover:scale-105 active:scale-85 hover:bg-[#A1E6EB] uppercase" target="_blank"
+            className="mx-10 mt-5 flex justify-center items-center text-center text-white bg-secondary rounded-full px-10 py-3 max-sm:text-xl text-2xl bt font-extrabold duration-50 hover:scale-105 active:scale-85 hover:bg-[#A1E6EB] uppercase" target="_blank"
             href="https://forms.gle/c44xoGNcv5LuxBoj6"
           >
             Participant Sign Up
           </a>
           <a
-            className="mt-5 flex justify-center items-center text-center text-white bg-secondary rounded-full px-10 py-3 max-sm:text-xl text-2xl bt font-extrabold duration-50 hover:scale-105 active:scale-85 hover:bg-[#A1E6EB] uppercase" target="_blank"
+            className="mx-10 mt-5 flex justify-center items-center text-center text-white bg-primary rounded-full px-10 py-3 max-sm:text-xl text-2xl bt font-extrabold duration-50 hover:scale-105 active:scale-85 hover:hover:bg-[#8B5050] uppercase" target="_blank"
             href="mailto:wicsuoft@gmail.com"
           >
             Contact Us

47add57edd91a655f5fca3b9444f48407c50648c
tenko08 <unalai1027@gmail.com>
2025-02-20 16:31:41 -0500

    sponsor messages

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -58,10 +58,10 @@ export default function Home() {
           Title Sponsor:
         </h1>
         <div className="flex flex-col md:flex-row w-full justify-center items-center mb-20">
-          <Image src={SAP} alt="SAP" className="w-1/3 max-sm:w-1/2" />
+          <Image src={SAP} alt="SAP" className="w-1/4 max-sm:w-1/2" />
           <div className="sm:text-xl w-2/3 mx-auto pt-5 text-base lg:text-2xl">
             <p>
-              {`SAP is a global leader in enterprise application software, providing a range of solutions for businesses across various industries. With a focus on innovation and customer success, SAP helps organizations streamline operations, drive growth, and stay ahead in a rapidly evolving market. SAMPLE TEXT`}
+              {`As a global leader in enterprise applications and business AI, SAP stands at the nexus of business and technology. For over 50 years, organizations have trusted SAP to bring out their best by uniting business-critical operations spanning finance, procurement, HR, supply chain, and customer experience. The company’s integrated applications connect all parts of a business into an intelligent suite on a fully digital platform, thereby replacing the process-driven, legacy platform. Today, SAP has more than 230 million cloud users, more than 100 solutions covering all business functions, and the largest cloud portfolio of any provider.`}
             </p>
           </div>
         </div>
@@ -69,26 +69,26 @@ export default function Home() {
           Co-Sponsors:
         </h2>
         <div className="flex flex-col md:flex-row w-full justify-center items-center mb-20">
-          <Image src={UofTEntrepreneurship} alt="UofT Entrepreneurship" className="w-1/3 max-sm:w-1/2" />
+          <Image src={UofTEntrepreneurship} alt="UofT Entrepreneurship" className="w-1/4 max-sm:w-1/2" />
           <div className="sm:text-xl w-2/3 mx-auto pt-5 text-base lg:text-2xl">
             <p>
-              {`SAP is a global leader in enterprise application software, providing a range of solutions for businesses across various industries. With a focus on innovation and customer success, SAP helps organizations streamline operations, drive growth, and stay ahead in a rapidly evolving market. SAMPLE TEXT`}
+              {`The University of Toronto Entrepreneurship (UTE) community does much more than accelerate startups. We accelerate ideas, people, social impact, and technologies too. All while striving to create an environment that is inclusive and representative of the diverse and talented innovators and entrepreneurs around us. UTE is a central voice for entrepreneurship across U of T’s three campuses and represents a powerful network of 12+ accelerators. We are here to support, develop and celebrate our best and brightest founders and their startups through a broad range of resources, network access and entrepreneurship activities.`}
             </p>
           </div>
         </div>
         <div className="flex flex-col md:flex-row w-full justify-center items-center max-sm:mb-20">
-          <Image src={Nowit} alt="Nowit" className="w-1/3 max-sm:w-1/2" />
+          <Image src={Nowit} alt="Nowit" className="w-1/4 max-sm:w-1/2" />
           <div className="sm:text-xl w-2/3 mx-auto pt-5 text-base lg:text-2xl">
             <p>
-              {`SAP is a global leader in enterprise application software, providing a range of solutions for businesses across various industries. With a focus on innovation and customer success, SAP helps organizations streamline operations, drive growth, and stay ahead in a rapidly evolving market. SAMPLE TEXT`}
+              {`NowIT is a boutique IT Managed Services and CFO/Controller firm located in Toronto’s Fashion Disctrict. With years of experience, our engineers have the expertise across all enterprise-level operating systems, applications and platforms. We offer timely support for Windows and macOS desktops, notebooks and as well as servers and network equipment.`}
             </p>
           </div>
         </div>
         <div className="flex flex-col md:flex-row w-full justify-center items-center">
-          <Image src={Scotiabank} alt="Scotiabank" className="w-1/3 max-sm:w-1/2" />
+          <Image src={Scotiabank} alt="Scotiabank" className="w-1/4 max-sm:w-1/2" />
           <div className="sm:text-xl w-2/3 mx-auto pt-5 text-base lg:text-2xl">
             <p>
-              {`SAP is a global leader in enterprise application software, providing a range of solutions for businesses across various industries. With a focus on innovation and customer success, SAP helps organizations streamline operations, drive growth, and stay ahead in a rapidly evolving market. SAMPLE TEXT`}
+              {`As one of Canada’s largest banks with a strong presence across the Americas, Scotiabank’s vision is to be our clients’ most trusted financial partner, to deliver sustainable, profitable growth and maximize total shareholder return. Guided by our purpose: "for every future," we help our clients, their families and their communities achieve success through a broad range of advice, products, and services, including personal and commercial banking, wealth management and private banking, corporate and investment banking, and capital markets.`}
             </p>
           </div>
         </div>

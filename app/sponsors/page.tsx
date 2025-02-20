98a8b152e8311af530182e215de6e4a644b379a8
tenko08 <unalai1027@gmail.com>
2025-02-20 15:59:10 -0500

    sponsorship text

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -1,6 +1,7 @@
 import MaxWidthWrapper from "../components/MaxWidthWrapper";
+import Image from "next/image";
 
-
+import SAP from "../assets/logos/sap.png";
 
 export default function Home() {
   return (
@@ -14,12 +15,44 @@ export default function Home() {
           </h1>
         </div>
       </div>
-      <MaxWidthWrapper>
-        {/* Hero section */}
-        <h1 className="mt-20 max-sm:text-3xl max-sm:text-center text-primary text-3xl font-bold max-md:text-5xl text-center">
-          🚀✨ Coming Soon... ✨🚀
+      <MaxWidthWrapper className="pt-8 text-center">
+        <h1 className="mt-16 text-3xl lg:text-5xl text-primary font-bold">
+          Support the future of Ethical AI
         </h1>
-        </MaxWidthWrapper>
+        <div className="sm:text-xl w-50 mx-auto pt-5 text-base lg:text-2xl">
+          <p>
+            {`The UofT AI Ethics Hackathon 2025 brings together innovative thinkers—developers, designers, and problem-solvers—to tackle pressing ethical challenges in AI. Hosted at the University of Toronto, this event fosters collaboration, critical discussion, and groundbreaking ideas on responsible AI development.`}
+            <br />
+            <br />
+            {`As a sponsor, you'll gain direct access to top talent, showcase your commitment to ethical AI, and connect with future industry leaders. Engage with participants beyond the hackathon weekend through mentorship, networking, and branding opportunities.`}
+            <br />
+            <br />
+            {`Join us in shaping the future of AI ethics. Let’s build a responsible AI-driven world together!`}
+            <br />
+            <br />
+            {`Interested in partnering with us?`}
+            <br />
+          </p>
+        </div>
+        <div className="mt-10 flex flex-col md:flex-row w-full justify-center items-center">
+          <a
+            className="mx-10 mt-5 flex justify-center items-center text-center text-white bg-primary rounded-full px-10 py-3 max-sm:text-xl text-2xl bt font-extrabold duration-50 hover:scale-105 active:scale-85 hover:hover:bg-[#8B5050] uppercase" target="_blank"
+            href="https://drive.google.com/file/d/16Oy4saDTpofk3Osw5KNPfw_G7COkt10m/view?usp=sharing"
+          >
+            sponsorship package
+          </a>
+          <a
+            className="mx-10 mt-5 flex justify-center items-center text-center text-white bg-secondary rounded-full px-10 py-3 max-sm:text-xl text-2xl bt font-extrabold duration-50 hover:scale-105 active:scale-85 hover:bg-[#A1E6EB] uppercase" target="_blank"
+            href="mailto:wicsuoft@gmail.com"
+          >
+            contact us
+          </a>
+        </div>
+
+        <hr className="bg-primary w-1/3 h-[2px] mx-auto mt-10"></hr>
+
+        
+      </MaxWidthWrapper>
     </main>
   );
 }
